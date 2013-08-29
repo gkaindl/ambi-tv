a163bdd7dc1289639b409ec5e730ef6a0a978a60
Kris <sn00zerman@users.noreply.github.com>
2013-08-29 14:54:53 +0200

    Create sbs-color-processor.h
    3D side-by-side for ambi-tv

---
+++ src/components/sbs-color-processor.h
@@ -0,0 +1,28 @@
+/* ambi-tv: a flexible ambilight clone for embedded linux
+*  Copyright (C) 2013 Georg Kaindl
+*  
+*  This file is part of ambi-tv.
+*  
+*  ambi-tv is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 2 of the License, or
+*  (at your option) any later version.
+*  
+*  ambi-tv is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  
+*  You should have received a copy of the GNU General Public License
+*  along with ambi-tv.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __AMBITV_SBS_COLOR_PROCESSOR_H__
+#define __AMBITV_SBS_COLOR_PROCESSOR_H__
+
+#include "../component.h"
+
+struct ambitv_processor_component*
+ambitv_sbs_color_processor_create(const char* name, int argc, char** argv);
+
+#endif // __AMBITV_SBS_COLOR_PROCESSOR_H__
